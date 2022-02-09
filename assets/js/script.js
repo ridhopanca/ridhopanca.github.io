4d1f11c833efac4eeba28fc72836511ae52e6901
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2022-02-09 16:11:57 +0700

    default

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    menuItem();
+});
+function menuItem(){
+	$('#menu li:has(ul)').children('ul').hide();
+	$('#menu li:has(ul)').find('a').on('click', function() {
+
+		var parent = $(this).parent(),
+		submenu = $(this).next('ul');
+
+		if (submenu.is(':visible')) {
+			$(this).parent().find('ul').slideUp(300);
+		}
+
+		if (submenu.is(':hidden')) {
+			parent.siblings().find('ul').slideUp(300);
+			parent.find('ul:first').slideDown(300);
+		}
+
+		if (parent.find('ul').children().length == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	});
+
+	$('#menu-burger').on('click', function() {
+		if (!$('#poppin-nav').hasClass('open')) {
+			toggle_nav(true);
+		} else {
+			toggle_nav(false);
+		}
+	});
+
+	$('#nav-off, #menu-overlay').on('click', function() {
+		toggle_nav(false);
+	});
+
+	function toggle_nav(lolly) {
+		if (lolly === true) {
+			$('#poppin-nav, #menu-overlay').addClass('nav-on');
+		} else {
+			$('#poppin-nav, #menu-overlay').removeClass('nav-on');
+
+			setTimeout(function() {
+				$('#menu li.has-submenu a').next('.submenu').slideUp(300);
+			}, 500);
+		}
+	}
+
+	$('.hamburger').on('click', function() {
+		$(this).toggleClass('is-active');
+		$(this).next().toggleClass('nav-show')
+	});
+	$('.menu-button a').on('click', function() {
+		$('.overlay').fadeToggle(300);
+		$(this).toggleClass('btn-open').toggleClass('btn-close');
+	});
+	$('.overlay').on('click', function() {
+		$('.menu-button').fadeToggle(300);
+		$('.button a').toggleClass('btn-open').toggleClass('btn-close');
+		open = false;
+	});
+
+	$(".button a").on('click', function() {
+		$(".overlay").fadeToggle(200);
+		$(this).toggleClass("btn-open").toggleClass("btn-close");
+	});
+
+	$(".overlay").on("click", function() {
+		$(".overlay").fadeToggle(200);
+		$(".button a").toggleClass("btn-open").toggleClass("btn-close");
+		open = false;
+	});
+}
+
+var selector = '#tab li a';
+
+$(selector).on('click', function(e){
+	e.preventDefault();
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
